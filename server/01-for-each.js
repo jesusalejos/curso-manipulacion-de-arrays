cafc30cc5383769519fb81bd814f39c3580648c5
Jesús Alejos <jalejos6@gmail.com>
2022-05-26 08:42:55 -0430

    Class for each

--- server/01-for-each.js
+++ server/01-for-each.js
@@ -2,7 +2,7 @@ const letters = ['a','b','c'];
 
 for (let index = 0; index < letters.length; index++) {
     const element = letters[index];
-    console.log('for',element);
+    console.log('Madi with for',element);
 }
 
-letters.forEach(item => console.log('forEach', item))
+letters.forEach(item => console.log('Made with forEach', item))
