99a0e77d0a95e041dc7c791f0ee6aeb9b87599c3
Jesús Alejos <jalejos6@gmail.com>
2022-05-28 07:27:35 -0430

    Challenge reduce

--- server/06-reduce-reloaded.js
+++ server/06-reduce-reloaded.js
@@ -4,7 +4,7 @@ const rta = items.reduce((obj, item) => {
     if (!obj[item]) {
         obj[item] = 1;
     } else {
-        obj[item] = obj[item] + 1;
+        obj[item]++;
     }
     return obj;
 }, {});
@@ -44,9 +44,37 @@ const rta1 = data
     if (!obj[item]) {
         obj[item] = 1;
     } else {
-        obj[item] = obj[item] + 1;
+        obj[item]++;
     }
     return obj;
 }, {});
 
 console.log(rta1);
+
+const range = [1, 3, 12,13,14,20,15,16,17,15,19,11,18, 2, 3,4,7,9,6,10,12,13,14,20,15,16,17,15,19,11,18];
+
+
+
+const rta2 = range.reduce ((obj,item) =>{
+  
+  if (item <= 5) {
+    obj.primerR = obj.primerR+1;
+  }
+
+  else if (item > 5 && item <=10) {
+    obj.segundoR = obj.segundoR+1;
+  }
+
+  else if (item > 10 && item <=15) {
+    obj.tercerR = obj.tercerR+1;
+  }
+
+  else if (item > 15 && item <=20) {
+    obj.cuartoR = obj.cuartoR+1;
+  }
+
+  return obj
+
+},{primerR:0,segundoR:0,tercerR:0,cuartoR:0});
+
+console.log(rta2);
