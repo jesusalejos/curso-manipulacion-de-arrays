dc56723fe0ff28e1a113a14ffb1674b24f79d22b
Jesús Alejos <jalejos6@gmail.com>
2022-05-28 08:57:23 -0430

    Challenge every

--- server/09-every.js
+++ server/09-every.js
@@ -28,6 +28,19 @@ const team = [
   },
   {
     name: "Santiago",
-    age: 18,
+    age: 15,
   },
 ];
+
+const rta3 = team.every((item)=>item.age<=15);
+console.log("rta3", rta3);
+
+function solution(numbers) {
+	const rta = numbers.every((item) => item % 2 === 0);
+
+	return rta
+};
+
+console.log(solution([2, 4, 6, 8, 10]));
+console.log(solution([1, 3, 5, 7, 10, 11]));
+solution([1, 3, 5]);
