d5a78313d599e47ac23dfffd5d59a55a960af4f2
Jesús Alejos <jalejos6@gmail.com>
2022-05-28 09:45:56 -0430

    Challenge includes

--- server/11-includes.js
+++ server/11-includes.js
@@ -12,3 +12,13 @@ for (let index = 0; index < pets.length; index++) {
 const rta = pets.includes('dog')
 console.log('for', includeInArray);
 console.log('includes', rta);
+
+function solution(words, query) {
+	const rta5 = words.filter(item => {
+		return item.includes(query);
+	});
+	return rta5;
+};
+
+console.log(solution(["ana", "santi", "nico", "anastasia"], "an"));
+console.log(solution(["ana", "santi", "nico", "anastasia"], "xyz"));
