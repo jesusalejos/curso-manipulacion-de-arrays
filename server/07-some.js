2d97b44557d5ec366a450dc7bfab45eabecdc18d
Jesús Alejos <jalejos6@gmail.com>
2022-05-28 08:14:25 -0430

    Challenge some

--- server/07-some.js
+++ server/07-some.js
@@ -79,3 +79,12 @@ const isOverlap = (newDate) => {
 }
 
 console.log('isOverlap', isOverlap(newAppointment));
+
+function solution(numbers) {
+	const rta = numbers.some((item) => item % 2 === 0); 
+
+    return rta
+};
+
+console.log(solution([1, 3, 5, 7, 10, 11]));
+solution([1, 3, 5]);
