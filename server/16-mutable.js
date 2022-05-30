d91faba29992d3bcf5b3fbc443f579a31cf7aff0
Jesús Alejos <jalejos6@gmail.com>
2022-05-30 13:24:32 -0430

    Finishing the course about the arrays

--- server/16-mutable.js
+++ server/16-mutable.js
@@ -1,22 +1,80 @@
-const products = [
-  { title: "Pizza", price: 121, id: "🍕" },
-  { title: "Burger", price: 121, id: "🍔" },
-  { title: "Hot cakes", price: 121, id: "🥞" },
-];
+// const products = [
+//   { title: "Pizza", price: 121, id: "🍕" },
+//   { title: "Burger", price: 121, id: "🍔" },
+//   { title: "Hot cakes", price: 121, id: "🥞" },
+// ];
+// const myProducts = [];
+// console.log("products", products);
+// console.log("myProducts", myProducts);
+// console.log("-".repeat(10));
+
+// const productIndex = products.findIndex(item => item.id === '🍔');
+// if (productIndex !== -1) {
+//     myProducts.push(products[productIndex]);
+//     products.splice(productIndex, 1);
+// }
+// console.log("products", products);
+// console.log("myProducts", myProducts);
+// console.log("-".repeat(10));
+
+// Challenge 1
+
+
 const myProducts = [];
-console.log("products", products);
-console.log("myProducts", myProducts);
-console.log("-".repeat(10));
-const productIndex = products.findIndex(item => item.id === '🍔');
-if (productIndex !== -1) {
-    myProducts.push(products[productIndex]);
-    products.splice(productIndex, 1);
-}
-console.log("products", products);
-console.log("myProducts", myProducts);
-console.log("-".repeat(10));
+let product_without_modified;
+const products = [
+    { title: "Pizza", price: 121, id: "🍕" },
+    { title: "Burger", price: 121, id: "🍔" },
+    { title: "Hot cakes", price: 121, id: "🥞" },
+  ];
+
+//   const product_modified = products.map(item=> item);
+//   console.log("modified", product_modified);
+
+  
+  console.log("products", products);
+  console.log("myProducts", myProducts);
+  console.log("-".repeat(10));
+  
+  const productIndex = products.findIndex(item => item.id === '🍔');
+  
+  if (productIndex !== -1) {
+        myProducts.push(products[productIndex]);
+        product_without_modified = products.map(item=> item);
+        products.splice(productIndex, 1);
+  }
+  console.log("products", products);
+  console.log("myProducts", myProducts);
+  console.log("Without_modified", product_without_modified);
+  console.log("-".repeat(10));
 
 // Update
+
+// const productsV2 = [
+//     { title: "Pizza", price: 121, id: "🍕" },
+//     { title: "Burger", price: 121, id: "🍔" },
+//     { title: "Hot cakes", price: 121, id: "🥞" },
+//   ];
+// const update = {
+//     id: "🥞",
+//     changes: {
+//         price: 200,
+//         description: 'delicioso'
+//     }
+// }
+
+// const productsV2M = productsV2.map(item => item);
+
+// // console.log(productsV2M);
+
+// const productIndexV2 = productsV2.findIndex(item => item.id === update.id);
+// productsV2[productIndexV2] = {
+//     ...productsV2[productIndexV2],
+//     ...update.changes,
+// };
+// console.log(productsV2);
+
+// Challenge 2
 
 const productsV2 = [
     { title: "Pizza", price: 121, id: "🍕" },
@@ -30,9 +88,16 @@ const update = {
         description: 'delicioso'
     }
 }
-const productIndexV2 = productsV2.findIndex(item => item.id === update.id);
-productsV2[productIndexV2] = {
-    ...productsV2[productIndexV2],
+
+const productsV2M = productsV2.map(item => item);
+
+// console.log(productsV2M);
+
+const productIndexV2 = productsV2M.findIndex(item => item.id === update.id);
+
+productsV2M[productIndexV2] = {
+    ...productsV2M[productIndexV2],
     ...update.changes,
 };
 console.log(productsV2);
+console.log(productsV2M);
--- server/17-sort.js
+++ server/17-sort.js
@@ -1,12 +1,15 @@
 const months = ['March', 'Jan', 'Feb', 'Dec'];
 months.sort();
 console.log(months);
+
 const numbers = [1, 30, 4, 21, 100000];
 numbers.sort((a,b) => b - a);
 console.log(numbers);
+
 const words = ['réservé', 'premier', 'communiqué', 'café', 'adieu', 'éclair', 'banana'];
 words.sort((a,b) => a.localeCompare(b));
 console.log(words);
+
 const orders = [
     {
       customerName: "Nicolas",
